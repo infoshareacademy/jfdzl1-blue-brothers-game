4ab187161c08a024ca17134fd9316186e3278713
Wojciech Paluch <wojciech.paluch@o2.pl>
2017-11-18 11:08:21 +0100

    added function wasted

--- js/main.js
+++ js/main.js
@@ -1,87 +1,89 @@
-
 var map = [
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 ],
-    [0, 9, 10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,0 ],
-    [0, 10,1, 0, 0, 0, 0, 0, 6, 10,5, 10,8, 0, 0, 0, 0, 0, 2, 10,0 ],
-    [0, 10,0, 10,10,10,10,10,10,10,0, 10,10,10,10,10,10,10,0, 10,0 ],
-    [0, 10,0, 11,8, 0, 0, 0, 6, 10,0, 10,8, 0, 0, 0, 6, 11,0, 10,0 ],
-    [0, 10,0, 10,10,10,10,10,10,10,10, 10,10,10,10,10,10,10,0, 10,0 ],
-    [0, 10,4, 0, 0, 0, 0, 0, 6, 10,0, 10,8, 0, 0, 0, 0, 0, 3, 10,0 ],
-    [0, 10,10,10,10,10,10,10,10,10,0, 10,10,10,10,10,10,10,10,10,0 ],
-    [0, 10,1, 0, 0, 0, 0, 0, 6, 10,0, 10,8, 0, 0, 0, 0, 0, 2, 10,0 ],
-    [0, 10,0, 10,10,10,10,10,10,10,0, 10,10,10,10,10,10,10,0, 10,0 ],
-    [0, 10,0, 11,8, 0, 0, 0, 6, 10,0, 10,8, 0, 0, 0, 6, 11,0, 10,0 ],
-    [0, 10,0, 10,10,10,10,10,10,10,10, 10,10,10,10,10,10,10,0, 10,0 ],
-    [0, 10,4, 0, 0, 0, 0, 0, 6, 10,7, 10,8, 0, 0, 0, 0, 0, 3, 10,0 ],
-    [0, 11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,0 ],
-    [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 ],
-    [0, 11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,0 ],
-    [0, 10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,11,0 ],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 ]
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+    [0, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0],
+    [0, 10, 1, 0, 0, 0, 0, 0, 6, 10, 5, 10, 8, 0, 0, 0, 0, 0, 2, 10, 0],
+    [0, 10, 0, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10, 0, 10, 0],
+    [0, 10, 0, 11, 8, 0, 0, 0, 6, 10, 0, 10, 8, 0, 0, 0, 6, 11, 0, 10, 0],
+    [0, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0, 10, 0],
+    [0, 10, 4, 0, 0, 0, 0, 0, 6, 10, 0, 10, 8, 0, 0, 0, 0, 0, 3, 10, 0],
+    [0, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0],
+    [0, 10, 1, 0, 0, 0, 0, 0, 6, 10, 0, 10, 8, 0, 0, 0, 0, 0, 2, 10, 0],
+    [0, 10, 0, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10, 0, 10, 0],
+    [0, 10, 0, 11, 8, 0, 0, 0, 6, 10, 0, 10, 8, 0, 0, 0, 6, 11, 0, 10, 0],
+    [0, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0, 10, 0],
+    [0, 10, 4, 0, 0, 0, 0, 0, 6, 10, 7, 10, 8, 0, 0, 0, 0, 0, 3, 10, 0],
+    [0, 11, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0],
+    [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+    [0, 11, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0],
+    [0, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3]
 
 ]
 var car = {
     x: 1,
     y: 1
 }
-var score = 0 ;
+var score = 0;
 
 
 var police = {
     x: 19,
     y: 13
+
 }
 
 
-function displayCar(){
-    document.getElementById('car').style.left = car.x*19+"px";
-    document.getElementById('car').style.top = car.y*20+"px";
+function displayCar() {
+    document.getElementById('car').style.left = car.x * 19 + "px";
+    document.getElementById('car').style.top = car.y * 20 + "px";
 }
 
-function displayPolice(){
-    document.getElementById('police').style.left = police.x*20+"px";
-    document.getElementById('police').style.top = police.y*20+"px";
+function displayPolice() {
+    document.getElementById('police').style.left = police.x * 20 + "px";
+    document.getElementById('police').style.top = police.y * 20 + "px";
 }
 
 
-function displayScore(){
+function displayScore() {
     document.getElementById('score').innerHTML = score;
 }
-function displayMap(){
+
+function displayMap() {
     var output = '';
-    for(var i=0; i<map.length; i++){
+    for (var i = 0; i < map.length; i++) {
         output += "\n<div class='row'>\n"
-        for(var j=0; j<map[i].length; j++){
-            if(map[i][j] == 0)
+        for (var j = 0; j < map[i].length; j++) {
+            if (map[i][j] == 0)
                 output += "<div class='brick'></div>";
-            else if(map[i][j] == 1)
+            else if (map[i][j] == 1)
                 output += "<div class='brick lefttop'></div>";
-            else if(map[i][j] == 2)
+            else if (map[i][j] == 2)
                 output += "<div class='brick righttop'></div>";
-            else if(map[i][j] == 3)
+            else if (map[i][j] == 3)
                 output += "<div class='brick rightbottom'></div>";
-            else if(map[i][j] == 4)
+            else if (map[i][j] == 4)
                 output += "<div class='brick leftbottom'></div>";
-            else if(map[i][j] == 5)
+            else if (map[i][j] == 5)
                 output += "<div class='brick top'></div>";
-            else if(map[i][j] == 6)
+            else if (map[i][j] == 6)
                 output += "<div class='brick right'></div>";
-            else if(map[i][j] == 7)
+            else if (map[i][j] == 7)
                 output += "<div class='brick bottom'></div>";
-            else if(map[i][j] == 8)
+            else if (map[i][j] == 8)
                 output += "<div class='brick left'></div>";
-            else if(map[i][j] == 9)
+            else if (map[i][j] == 9)
                 output += "<div class='empty'></div>";
-            else if(map[i][j] == 10)
+            else if (map[i][j] == 10)
                 output += "<div class='road'></div>";
-            else if(map[i][j] == 11)
+            else if (map[i][j] == 11)
                 output += "<div class='fuel'></div>";
         }
         output += "\n</div>"
     }
     $('#map').html(output);
 }
-document.onkeydown = function(e) {
+
+document.onkeydown = function (e) {
     if (e.keyCode == 37 && (map[car.y][car.x - 1] == 9 || map[car.y][car.x - 1] == 10 || map[car.y][car.x - 1] == 11)) {
         $('#car').removeClass('right');
         $('#car').removeClass('up');
@@ -110,14 +112,16 @@ document.onkeydown = function(e) {
         $('#car').addClass('up');
         car.y++;
     }
-    if(map[car.y][car.x] == 11){
+    if (map[car.y][car.x] == 11) {
         map[car.y][car.x] = 9;
-        score+=50;
+        score += 50;
         displayMap();
         displayScore();
     }
-    displayCar()
-}
+    displayCar();
+    checkend()
+
+};
 
 
 function getRandom() {
@@ -162,12 +166,21 @@ function PoliceMove() {
 
     displayPolice();
 
+
 }
+
+function checkend() {
+    if ((car.x == police.x) && (car.y == police.y)) {
+        alert("Wasted!");
+    }
+}
+
 
 setInterval(PoliceMove, 500)
 
-$(document).ready(function(){
+$(document).ready(function () {
     displayMap();
     displayCar();
     displayScore();
-});
+    checkend()
+})
