ff754c5fe2114e8567c5f46e333226f5c2cb0f69
BartlomiejWrz <bartlomiej18@op.pl>
2017-11-19 09:52:37 +0100

    active policeman

--- js/main.js
+++ js/main.js
@@ -37,11 +37,11 @@ var police2 = {
     y: 13
 }
 
-/* trzecai policja pozycja
+// trzecai policja pozycja
 var police3 = {
     x: 19,
     y: 1
-}*/
+}
 
 
 
@@ -61,8 +61,8 @@ function displayPolice2() {
 }
 // trzecia policja wielkość diva
 function displayPolice3() {
-    document.getElementById('police3').style.left = police2.x * 20 + "px";
-    document.getElementById('police3').style.top = police2.y * 20 + "px";
+    document.getElementById('police3').style.left = police3.x * 20 + "px";
+    document.getElementById('police3').style.top = police3.y * 20 + "px";
 }
 
 
@@ -237,7 +237,7 @@ function PoliceMove2() {
     checkend();
 }
 
-/*trzecia policja ruch policji
+//trzecia policja ruch policji
 function PoliceMove3() {
     var newDirection = getRandom();
 
@@ -273,7 +273,7 @@ function PoliceMove3() {
 
     displayPolice3();
     checkend();
-}*/
+}
 
 
 
@@ -289,11 +289,11 @@ function checkend() {
 
     }
 
-    /*trzecia policja wysypanie się po zderzeniu
+    //trzecia policja wysypanie się po zderzeniu
     if ((car.x == police3.x) && (car.y == police3.y)) {
         gameover("Crashed");;
 
-    }*/
+    }
 
 
     if (score == 0)  {
@@ -321,7 +321,7 @@ setInterval(PoliceMove, 250)
 // druga policja szybkość porusania się
 setInterval(PoliceMove2, 250)
 // trzecia policja szybkość porusania się
-//setInterval(PoliceMove3, 250)
+setInterval(PoliceMove3, 250)
 
 
 $(document).ready(function () {
